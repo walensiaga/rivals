9af4510284977a9b6c1d5bb0d07b732fdac3b91f
walensiaga <welldscome@gmail.com>
2025-03-02 12:44:28 +0200

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,6 +1,3 @@
---[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local player = Players.LocalPlayer
